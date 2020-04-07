481d3d022eba3e25c3838cab11d26a68e477fc0a
SyedAsaad <asaadkhurshid96@gmail.com>
2020-04-07 15:56:13 +0500

    Adding component

---
+++ src/app/common/Dish.ts
@@ -0,0 +1,10 @@
+export class Dish{
+    id:String;
+    name:String;
+    image:String;
+    category:String;
+    featured:boolean;
+    label:String;
+    price:String;
+    description:String;
+}
